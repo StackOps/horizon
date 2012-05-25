1aac10a2bb4904b0f9f5314c3f6a071640ad42b2
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2012-05-25 09:48:22 +0200

    check snapshot size implement delete snapshot

--- django-openstack/django_openstack/dash/views/snapshots.py
+++ django-openstack/django_openstack/dash/views/snapshots.py
@@ -54,8 +54,18 @@ class CreateSnapshot(forms.SelfHandlingForm):
         try:
 	    resources = utils.get_resources(request, request.user.tenant_id)
 	    instance = api.server_get(request, data['instance_id'])
-	    if instance.attrs.disk_gb > resources['free_disk']:
-	        raise Exception('Not enough space to store the snapshot: image_size=%s free_disk=%s' % (instance.attrs.disk_gb, resources['free_disk']))
+	    disk_gb = 0
+	    all_images = []
+	    all_images = api.image_list_detailed(request)
+	    if not all_images:
+ 		messages.info(request, "There are currently no images.")
+	    images = [im for im in all_images
+              if im['container_format'] not in ['aki', 'ari']]
+	    for image in images:
+    		if int(image['id']) == int(instance.attrs.image_ref):
+		    disk_gb = image['size'] / (1024 * 1024 * 1024)
+	    if disk_gb > resources['free_disk']:
+	        raise Exception('Not enough space to store the snapshot: image_size=%s free_disk=%s' % (disk_gb, resources['free_disk']))
             LOG.info('Creating snapshot "%s"' % data['name'])
             snapshot = api.snapshot_create(request,
                     data['instance_id'],
@@ -65,15 +75,49 @@ class CreateSnapshot(forms.SelfHandlingForm):
             messages.info(request, 'Snapshot "%s" created for instance "%s"' %\
                                     (data['name'], instance.name))
             return shortcuts.redirect('dash_snapshots', data['tenant_id'])
+        except glance_exception.ClientConnectionError, e:
+            LOG.exception("Error connecting to glance")
+            messages.error(request, "Error connecting to glance: %s" % str(e))
+        except glance_exception.Error, e:
+            LOG.exception("Error retrieving image list")
+            messages.error(request, "Error retrieving image list: %s" % str(e))
         except api_exceptions.ApiException, e:
             msg = 'Error Creating Snapshot: %s' % e.message
             LOG.exception(msg)
             messages.error(request, msg)
             return shortcuts.redirect(request.build_absolute_uri())
 
+class DeleteImage(forms.SelfHandlingForm):
+    image_id = forms.CharField(required=True)
+
+    def handle(self, request, data):
+        image_id = data['image_id']
+        tenant_id = request.user.tenant_id
+        try:
+            image = api.image_get(request, image_id)
+            if image.owner == request.user.tenant_id:
+                api.image_delete(request, image_id)
+            else:
+                messages.info(request, "Unable to delete image, you are not \
+                                       its owner.")
+                return redirect('dash_images_update', tenant_id, image_id)
+        except glance_exception.ClientConnectionError, e:
+            LOG.exception("Error connecting to glance")
+            messages.error(request, "Error connecting to glance: %s"
+                                    % e.message)
+        except glance_exception.Error, e:
+            LOG.exception('Error deleting image with id "%s"' % image_id)
+            messages.error(request, "Error deleting image: %s: %s"
+                                    % (image_id, e.message))
+        return redirect(request.build_absolute_uri())
 
 @login_required
 def index(request, tenant_id):
+    for f in (DeleteImage, ):
+        unused, handled = f.maybe_handle(request)
+        if handled:
+            return handled
+    delete_form = DeleteImage()
     images = []
 
     try:
@@ -89,6 +133,7 @@ def index(request, tenant_id):
 
     return render_to_response(
     'django_openstack/dash/snapshots/index.html', {
+        'delete_form': delete_form,
         'images': images,
     }, context_instance=template.RequestContext(request))
 
